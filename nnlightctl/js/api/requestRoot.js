e45c1a05b296de67cce380fe119489d7ee0d3fd0
hjsg <hjsgwanghuan@163.com>
2019-07-09 10:46:12 +0800

    'text-wh'

--- nnlightctl/js/api/requestRoot.js
+++ nnlightctl/js/api/requestRoot.js
@@ -2,6 +2,7 @@ const BASE_ROOT = "http://103.48.232.119:9002/nnlightctl/";
 // const BASE_ROOT = "http://192.168.152.103:8080/webctl_war/"; // 闫鹏飞
 // const BASE_ROOT = "http://192.168.152.198:8090/nnlightctl/"; // 张华伟
 // const BASE_ROOT = "http://192.168.10.197:8080/nnlightctl/";
+
 const PAGE_SIZE = 10;
 
 const SEX = {
